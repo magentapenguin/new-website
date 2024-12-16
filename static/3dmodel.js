4fc19a37875f4c9abf94941bfa0085b8fd590c72
magentapenguin <83044109+magentapenguin@users.noreply.github.com>
2024-12-15 20:09:20 -0800

    Update 3D model and enhance rendering; switch model file, improve lighting, and add bloom effect

--- static/3dmodel.js
+++ static/3dmodel.js
@@ -3,7 +3,7 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
-import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
+import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js';
 import { SAOPass } from 'three/addons/postprocessing/SAOPass.js';
 import { TAARenderPass } from 'three/addons/postprocessing/TAARenderPass.js';
 import { OutputPass } from 'three/addons/postprocessing/OutputPass.js';
@@ -16,33 +16,39 @@ const container = document.getElementById('model');
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({ antialias: false });
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setClearColor(0xffffff, 0);
+renderer.setClearColor(0xffffff, 1);
 renderer.setSize(container.clientWidth, container.clientHeight);
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.autoRotate = true;
+
 const composer = new EffectComposer( renderer );
 const firstPass = new SAOPass( scene, camera );
 firstPass.params.saoBias = 0.5;
-firstPass.params.saoIntensity = 0.1;
+firstPass.params.saoIntensity = 0.15;
 firstPass.params.saoScale = 100;
 firstPass.params.saoKernelRadius = 100;
 firstPass.params.saoMinResolution = 0;
+const bloomPass = new UnrealBloomPass( new THREE.Vector2( container.clientWidth, container.clientHeight ), 1.5, 0.4, 0.85 );
+bloomPass.threshold = 1;
+bloomPass.strength = 0.3;
+bloomPass.radius = 0.2;
 const renderPass = new TAARenderPass( scene, camera );
 const outputPass = new OutputPass();
+
 composer.addPass( renderPass );
 composer.addPass( firstPass );
+composer.addPass( bloomPass );
 composer.addPass( outputPass );
 
-const light = new THREE.AmbientLight(0xffffff, 1);
+const light = new THREE.AmbientLight(0xffffff, 5);
 scene.add(light);
-const pointlight1 = new THREE.PointLight(0xffffff, 30);
-pointlight1.position.set(0, 4, 0);
+const pointlight1 = new THREE.PointLight(0xffffff, 40);
+pointlight1.position.set(3, 4, 0);
 scene.add(pointlight1);
-const pointlight2 = new THREE.SpotLight(0xffffff, 60);
-pointlight2.position.set(0, 4, 7);
-scene.add(pointlight2);
+const light2 = new THREE.RectAreaLight(0xffffff, 60);
+light2.position.set(0, 4, 7);
+scene.add(light2);
 // floor plane
 const geometry = new THREE.PlaneGeometry(10, 10, 10, 10);
 const material = new THREE.MeshLambertMaterial({ color: 0x888888, side: THREE.DoubleSide});
@@ -63,6 +69,7 @@ loader.load(container.dataset.file, function (gltf) {
     scene.add(gltf.scene);
     model = gltf.scene;
     model.position.set(0, 0, 0);
+    model.scale.set(0.1, 0.1, 0.1);
 }, undefined, function (error) {
     console.error(error);
 });
